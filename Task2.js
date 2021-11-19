df771fa81c794746d206f085c2250aa80fffe746
kadudhula sushmitha <kadudhulasushmitha@gmail.com>
2021-11-19 10:07:40 +0530

    hello

--- Task2.js
+++ Task2.js
@@ -4,7 +4,7 @@ function findFreq(str, char) {
     let count = 0;
     for (let i = 0; i < str.length; i++) {
         if (str.charAt(i) == char) {
-            count += 1;
+            count =count + 1;
         }
     }
 
